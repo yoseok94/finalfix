6b489c29b6eee6324cc1de97e5c857954c103734
Yun-YoungKwang <96574203+YeongKwang-Yun@users.noreply.github.com>
2023-04-26 12:11:44 +0900

    Update DashBoard.vue

--- src/views/common/DashBoard.vue
+++ src/views/common/DashBoard.vue
@@ -1,46 +1,48 @@
 <template>
-<div class="dashboard">
-     <div class="dashboard-row">
-       <div class="dashboard-column">
-         공지사항
-       </div>
-       <div class="dashboard-column">
+  <div class="dashboard">
+    <div class="dashboard-row">
+      <div class="dashboard-column">
+        공지사항
+      </div>
+      <div class="dashboard-column">
         이벤트페이지
       </div>
-     </div>
-          <div class="dashboard-row">
-       <div class="dashboard-column">
-        영업계획표(켈린더)
-       </div>
-       <div class="dashboard-column">
-         월별 매출 그래프
-       </div>
-     </div>
     </div>
- </template>
- 
- <script>
- 
- </script>
+    <div class="dashboard-row">
+      <div class="dashboard-column">
+        영업 계획표(켈린더)
+      </div>
+      <div class="dashboard-column">
+        매출 그래프(월별, 일별)
+      </div>
+    </div>
+  </div>
+</template>
 
- <style>
+<script>
+</script>
 
+<style>
 .dashboard {
-    display: flex;
-    flex-direction: row;
-
-}
-.dashboard-row {
   display: flex;
   flex-direction: column;
+  height: calc(100vh - 60px);
+}
+
+.dashboard-row {
+  display: flex;
+  flex-direction: row;
   width: 100%;
-  align-content: center;
+  height: 50%;
+}
+
+.dashboard-column {
+  display: flex;
   align-items: center;
   justify-content: center;
-
-}
-.dashboard-column {
+  width: 50%;
+  height: 100%;
   border: 3px dashed red;
+  overflow: auto;
 }
-
 </style>
