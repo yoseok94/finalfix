332e8543f271f5bd0cb8656b7c0c655a3ff08062
kangyongbeom <solra5090@naver.com>
2023-04-28 10:10:13 +0900

    업업관리
    영업지출결의서(관리자)/영업지출결의서 발송(관리자)추가
    대시보드(캘린더)추가
    영업계획(캘린더)추가

---
+++ src/views/business/BusinessDisbursementAdminSend.vue
@@ -0,0 +1,109 @@
+<template>
+    <div class="expense-form">
+      <div class="header">
+        <h2>영업지출 결의서 발송</h2>
+      </div>
+      <table class="expense-info">
+        <tbody>
+          <tr>
+            <th>결제금액</th>
+            <td>1,100,000</td>
+            <th>발송일</th>
+            <td>2020/01/01</td>
+          </tr>
+          <tr>
+            <th>발송인</th>
+            <td>관리자1</td>
+            <th>지급방법</th>
+            <td>
+              <label for="card"><input type="checkbox" id="card"> 카드</label>
+              <label for="cash"><input type="checkbox" id="cash"> 현금</label>
+              <label for="transfer"><input type="checkbox" id="transfer"> 계좌이체</label>
+            </td>
+          </tr>
+          <tr>
+            <th>거래처</th>
+            <th>상세내역</th>
+            <th>금액</th>
+            <th>부가세</th>
+          </tr>
+          <tr>
+            <td>거래처1</td>
+            <td>테스트1</td>
+            <td>1,000,000</td>
+            <td>100,000</td>
+          </tr>
+          <tr>
+            <td></td>
+            <th>합계</th>
+            <td>1,000,000</td>
+            <td>100,000</td>
+          </tr>
+          <tr>
+            <th>비고</th>
+            <td colspan="4"></td>
+          </tr>
+        </tbody>
+      </table>
+      <div class="buttons">
+        <button id="send">발송</button>
+        <button id="cancel">취소</button>
+      </div>
+    </div>
+  </template>
+  <style scoped>
+    .expense-form {
+      font-family: Arial, Helvetica, sans-serif;
+      font-size: 14px;
+      line-height: 1.5;
+      color: #333;
+    }
+  .header {
+  text-align: center;
+  margin-bottom: 30px;
+  }
+table {
+border-collapse: collapse;
+width: 100%;
+margin-bottom: 30px;
+}
+th,
+td {
+border: 1px solid #333;
+padding: 8px;
+text-align: center;
+}
+th {
+background-color: #f5f5f5;
+font-weight: bold;
+}
+tbody tr:nth-child(even) {
+background-color: #f5f5f5;
+}
+
+label {
+margin-right: 10px;
+}
+
+.buttons {
+display: flex;
+justify-content: center;
+margin-top: 20px;
+}
+
+button {
+    padding: 8px 16px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  cursor: pointer;
+  margin-left: 8px;
+  border-radius: 4px;
+  font-size: 14px;
+}
+
+button:hover {
+background-color: #333;
+color: #f5f5f5;
+}
+</style>
--- src/views/business/BusinessPlanCalendar.vue
+++ src/views/business/BusinessPlanCalendar.vue
@@ -1,149 +1,32 @@
-<template>
-    <div class="calendar">
-      <h2>
-        <a href="#" @click="onClickPrev(currentMonth)">◀</a>
-        {{ currentYear }}년 {{ currentMonth }}월
-        <a href="#" @click="onClickNext(currentMonth)">▶</a>
-      </h2>
-      <table class="table table-hover">
-        <thead>
-          <tr>
-            <td v-for="(weekName, index) in weekNames" :key="index">
-              {{ weekName }}
-            </td>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(row, index) in currentCalendarMatrix" :key="index">
-            <td
-              v-for="(day, index2) in row"
-              :key="index2"
-              style="padding: 20px;"
-            >
-              <span v-if="isToday(currentYear, currentMonth, day)" class="rounded">
-                {{ day }}
-              </span>
-              <span v-else>
-                {{ day }}
-              </span>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </template>
-  <script>
-  export default {
-    // eslint-disable-next-line vue/multi-word-component-names
-    name: "Calendar",
-    data() {
-      return {
-        weekNames: ["월요일", "화요일", "수요일", "목요일", "금요일", "토요일", "일요일"],
-        rootYear: 1904,
-        rootDayOfWeekIndex: 6, // 1904년 1월 1일은 토요일
-        currentYear: new Date().getFullYear(),
-        currentMonth: new Date().getMonth() + 1,
-        currentDay: new Date().getDate(),
-        currentMonthStartWeekIndex: null,
-        currentCalendarMatrix: [],
-        endOfDay: null,
-        memoDatas: [],
-      };
-    },
-    mounted() {
-      this.init();
-    },
-    methods: {
-      init() {
-        this.currentMonthStartWeekIndex = this.getStartWeek(
-          this.currentYear,
-          this.currentMonth
-        );
-        this.endOfDay = this.getEndOfDay(this.currentYear, this.currentMonth);
-        this.initCalendar();
-      },
-      initCalendar() {
-        this.currentCalendarMatrix = [];
-        let day = 1;
-        for (let i = 0; i < 6; i++) {
-          let calendarRow = [];
-          for (let j = 0; j < 7; j++) {
-            if (i === 0 && j < this.currentMonthStartWeekIndex) {
-              calendarRow.push("");
-            } else if (day <= this.endOfDay) {
-              calendarRow.push(day);
-              day++;
-            } else {
-              calendarRow.push("");
-            }
-          }
-          this.currentCalendarMatrix.push(calendarRow);
-        }
-      },
-      getEndOfDay(year, month) {
-        switch (month) {
-          case 2:
-            return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)
-              ? 29
-              : 28;
-          case 4:
-          case 6:
-          case 9:
-          case 11:
-            return 30;
-          default:
-            return 31;
-        }
-      },
-        getStartWeek: function(targetYear, targetMonth){
-          let year = this.rootYear;
-          let month = 1;
-          let sumOfDay = this.rootDayOfWeekIndex;
-          // eslint-disable-next-line no-constant-condition
-          while(true){
-            if(targetYear > year){
-              for(let i=0; i<12; i++){
-                sumOfDay += this.getEndOfDay(year, month+i);
-              }
-              year++;
-            }
-            else if(targetYear == year){
-              if(targetMonth > month){
-                sumOfDay += this.getEndOfDay(year, month);
-                month++;
-              }
-              else if(targetMonth == month){
-                return (sumOfDay)%7;
-              }
-            }
-          }
-        },
-        onClickPrev: function(month){
-          month--;
-          if(month<=0){
-            this.currentMonth = 12;
-            this.currentYear -= 1;
-          }
-          else{
-            this.currentMonth -= 1;
-          }
-          this.init();
-        },
-        onClickNext: function(month){
-          month++;
-          if(month>12){
-            this.currentMonth = 1;
-            this.currentYear += 1;
-          }
-          else{
-            this.currentMonth += 1;
-          }
-          this.init();
-        },
-        isToday: function(year, month, day){
-          let date = new Date();
-          return year == date.getFullYear() && month == date.getMonth()+1 && day == date.getDate(); 
-        }
+<script>
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+
+export default {
+  components: {
+    FullCalendar // make the <FullCalendar> tag available
+  },
+  data() {
+    return {
+      calendarOptions: {
+        plugins: [ dayGridPlugin, interactionPlugin ],
+        initialView: 'dayGridMonth',
+        dateClick: this.handleDateClick,
+        events: [
+          { title: 'event 1', date: '2019-04-01' },
+          { title: 'event 2', date: '2019-04-02' }
+        ]
+      }
+    }
+  },
+  methods: {
+    handleDateClick: function(arg) {
+      alert('date click! ' + arg.dateStr)
     }
   }
-  </script>
+}
+</script>
+<template>
+  <FullCalendar :options="calendarOptions" />
+</template>
